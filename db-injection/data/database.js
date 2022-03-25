ca00f81432f7de0da1ede78df862517e1ab56751
moff <illholdthemoff@gmail.com>
2022-03-24 20:22:06 -0400

    finished section

--- db-injection/data/database.js
+++ db-injection/data/database.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  database: 'security',
-  user: 'root',
-  password: 'your-pw',
-  multipleStatements: true
-})
+  host: "localhost",
+  database: "security",
+  user: "root",
+  password: "!som1%eof2a&poo1(patro4opa",
+  multipleStatements: true,
+});
 
 module.exports = pool;
